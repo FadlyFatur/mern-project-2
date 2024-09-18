fb0b8b345656fab821fc8432fd4fc24519787608
fdly <faturrohman309@gmail.com>
2024-09-18 14:52:38 +0700

    add home & about content

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='mb-28 text-5xl text-slate-800 font-bold'>This web build on..</h1>
+      <div className='flex gap-4'>
+        <img className='h-26 w-auto self-center object-cover mt-2' src="/asset/node+express.png" alt="node and express.js" />
+        <img className='h-26 w-auto self-center object-cover mt-2' src="/asset/react.png" alt="react.js" />
+        <img className='h-26 w-auto self-center object-cover mt-2' src="/asset/tailwind.png" alt="tailwind" />
+        <img className='h-26 w-auto self-center object-cover mt-2' src="/asset/redux.png" alt="redux" />
+      </div>
+    </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='font-bold text-3xl mb-4 text-slate-700'>Welcome to the Auth site App!</h1>
+      <p className='mb-4 text-slate-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis aliquam, veritatis eos cupiditate, quidem minima eligendi ratione repellat aperiam magnam consequatur molestiae. Modi voluptatum magnam id similique dolorem. Repellat, ab.</p>
+      <p className='mb-4 text-slate-800'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit dignissimos aliquam ab, eveniet magnam velit enim minus nemo ad quaerat nam sint officia, veniam exercitationem fugit eos sunt nihil consequuntur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet sequi aut, necessitatibus et ipsum ex repellat in eius voluptatum ipsa cum saepe corporis dolore id voluptas obcaecati, doloremque consectetur.</p>
+    </div>
   )
 }
